37184cab3227cb67357d58de85649c4b032b86e8
Moustapha Lo <moustafalo4555@gmail.com>
2020-11-17 21:55:35 +0100

    End to end test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -12,6 +12,32 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('angular-project app is running!');
   });
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    browser.pause();
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,8 +1,23 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
+  /*navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
+  }*/
+
+  navigateTo(link: string) {
+    return browser.get(link);
+  }
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
+  }
+
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
   }
 
   getTitleText(): Promise<string> {
